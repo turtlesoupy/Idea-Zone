f8158ee4788164a0d26842c7baf466e1956d1793
tdimson <tdimson@gmail.com>
2015-02-15 01:23:41 -0800

    Dummy AIB constants file

---
+++ Idea Box/AIBConstants.m
@@ -0,0 +1,19 @@
+//
+// Created by Thomas Dimson on 12/24/13.
+// Copyright (c) 2013 Thomas Dimson. All rights reserved.
+//
+
+#import "AIBConstants.h"
+
+NSString *const kAIBDropboxApiKey = @"FILLME";
+NSString *const kAIBDropboxApiSecret = @"FILLME";
+NSString * const kAIBTestflightApiKey = @"FILLME";
+NSString *const kAIBCrashlyticsKey = @"FILLME";
+
+NSString * const kAIBDropboxOpenURLOccurred = @"DropboxOpenUrlOccurred";
+NSString * const kAIBAppStoreId = @"795927539";
+
+@implementation AIBConstants {
+
+}
+@end
